0b3aedd29de287d6474ea87a95d0aa64330269aa
Ronald Gouldner <ronald.gouldner@gmail.com>
2025-02-14 09:07:51 -1000

    makelists script update

--- runMakeRandomLists.sh
+++ runMakeRandomLists.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/bash
+. /home/gouldner/.bashrc
+cd /home/gouldner/rons-spotify-randomizer
 source venv/bin/activate
-python3 MakeRandomLists.py
+python3 MakeRandomLists.py > MakeRandomLists.out 2> MakeRandomLists.err
